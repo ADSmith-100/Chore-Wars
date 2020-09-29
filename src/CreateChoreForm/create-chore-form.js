d7a18208766073ca55ac3bd743dd4fb239c2b7be
Adam Smith <coloradoasmith@gmail.com>
2020-09-28 20:17:27 -0600

    create chore form working

--- src/CreateChoreForm/create-chore-form.js
+++ src/CreateChoreForm/create-chore-form.js
@@ -1,118 +1,137 @@
 import React, { Component, useState } from "react";
 import { BrowserRouter, Route, Link, Router } from "react-router-dom";
-import $ from "jquery";
 import "./create-chore-form.css";
-import { render } from "@testing-library/react";
 
 export default function CreateChoreForm() {
-  const [inputList, setInputList] = useState([
-    { memberName: "", choreName: "" },
-  ]);
+  const [inputListMember, setInputListMember] = useState([{ memberName: "" }]);
+
+  const [inputListChore, setInputListChore] = useState([{ choreName: "" }]);
 
   // handle input change
-  const handleInputChange = (e, index) => {
+  const handleInputChangeMember = (e, index) => {
     const { name, value } = e.target;
-    const list = [...inputList];
+    const list = [...inputListMember];
     list[index][name] = value;
-    setInputList(list);
+    setInputListMember(list);
   };
 
   // handle click event of the Remove button
-  const handleRemoveClick = (index) => {
-    const list = [...inputList];
+  const handleRemoveClickMember = (index) => {
+    const list = [...inputListMember];
     list.splice(index, 1);
-    setInputList(list);
+    setInputListMember(list);
   };
 
   // handle click event of the Add button
-  const handleAddClick = () => {
-    setInputList([...inputList, { memberName: "", choreName: "" }]);
+  const handleAddClickMember = () => {
+    setInputListMember([...inputListMember, { memberName: "" }]);
   };
+
+  // handle input change
+
+  const handleInputChangeChore = (e, index) => {
+    const { name, value } = e.target;
+    const list = [...inputListChore];
+    list[index][name] = value;
+    setInputListChore(list);
+  };
+
+  // handle click event of the Remove button
+  const handleRemoveClickChore = (index) => {
+    const list = [...inputListChore];
+    list.splice(index, 1);
+    setInputListChore(list);
+  };
+
+  // handle click event of the Add button
+  const handleAddClickChore = () => {
+    setInputListChore([...inputListChore, { choreName: "" }]);
+  };
+
   return (
-    <div>
-      <h2>New Chore List Form</h2>
-      {inputList.map((x, i) => {
-        return (
-          <div className="chore-form">
-            <form id="add-chores-form">
-              <label>Household Name</label>
+    <div className="chore-form">
+      <form id="add-chores-form">
+        <label>Household Name</label>
+        <input
+          type="text"
+          name="household-name"
+          placeholder="The Smith's"
+          required
+        />
+
+        <div className="form-section">
+          <label>Chore Week Begins</label>
+          <input type="date" placeholder="yyyy-mm-dd" required />
+        </div>
+        <div className="form-section">
+          <label>Chore Week Ends</label>
+          <input type="date" placeholder="yyyy-mm-dd" required />
+        </div>
+        {inputListMember.map((x, i) => {
+          return (
+            <div className="member-name" id="add-member">
+              <label>Add Household Member</label>
               <input
+                id="first-member"
                 type="text"
-                name="household-name"
-                placeholder="The Smith's"
-                required
+                className="member-name"
+                placeholder="Larry"
+                onChange={(e) => handleInputChangeMember(e, i)}
               />
-
-              <div className="form-section">
-                <label>Chore Week Begins</label>
-                <input type="date" placeholder="yyyy-mm-dd" required />
+              <div className="btn-box">
+                {inputListMember.length !== 1 && (
+                  <button
+                    className="mr10"
+                    onClick={() => handleRemoveClickMember(i)}
+                  >
+                    Remove
+                  </button>
+                )}
+                {inputListMember.length - 1 === i && (
+                  <button onClick={handleAddClickMember}>Add</button>
+                )}
               </div>
-              <div className="form-section">
-                <label>Chore Week Ends</label>
-                <input type="date" placeholder="yyyy-mm-dd" required />
-              </div>
-              <div className="member-name" id="add-member">
-                <label>Add Household Member</label>
-                <input
-                  id="first-member"
-                  type="text"
-                  className="member-name"
-                  placeholder="Larry"
-                  onChange={(e) => handleInputChange(e, i)}
-                />
-                <div className="btn-box">
-                  {inputList.length !== 1 && (
-                    <button
-                      className="mr10"
-                      onClick={() => handleRemoveClick(i)}
-                    >
-                      Remove
-                    </button>
-                  )}
-                  {inputList.length - 1 === i && (
-                    <button onClick={handleAddClick}>Add</button>
-                  )}
-                </div>
-              </div>
-
-              <div className="chore-title" id="add-chore">
-                <label>Add New Chore</label>
-                <input
-                  id="first-chore"
-                  type="text"
-                  name="chore-title"
-                  placeholder="Dinner Dishes"
-                  onChange={(e) => handleInputChange(e, i)}
-                />
-                <div className="btn-box">
-                  {inputList.length !== 1 && (
-                    <button
-                      className="mr10"
-                      onClick={() => handleRemoveClick(i)}
-                    >
-                      Remove
-                    </button>
-                  )}
-                  {inputList.length - 1 === i && (
-                    <button onClick={handleAddClick}>Add</button>
-                  )}
-                </div>
-                <input
-                  type="checkbox"
-                  name="isDaily"
-                  id="isDaily"
-                  title="Daily Chore?"
-                >
-                  {/* Daily Chore? */}
-                </input>
+            </div>
+          );
+        })}
+        {inputListChore.map((x, i) => {
+          return (
+            <div className="chore-title" id="add-chore">
+              <label>Add New Chore</label>
+              <input
+                id="first-chore"
+                type="text"
+                name="chore-title"
+                placeholder="Dinner Dishes"
+                onChange={(e) => handleInputChangeChore(e, i)}
+              />
+              <div className="btn-box">
+                {inputListChore.length !== 1 && (
+                  <button
+                    className="mr10"
+                    onClick={() => handleRemoveClickChore(i)}
+                  >
+                    Remove
+                  </button>
+                )}
+                {inputListChore.length - 1 === i && (
+                  <button onClick={handleAddClickChore}>Add</button>
+                )}
               </div>
 
-              <button type="submit">Submit</button>
-              <button type="reset">Reset</button>
-            </form>
-          </div>
-        );
-      })}
+              <input
+                type="checkbox"
+                name="isDaily"
+                id="isDaily"
+                title="Daily Chore?"
+              ></input>
+              <label>Daily Chore?</label>
+            </div>
+          );
+        })}
+        <button type="submit">Submit</button>
+        <button type="reset">Reset</button>
+      </form>
     </div>
   );
 }
