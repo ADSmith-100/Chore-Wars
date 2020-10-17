2c2da2566aeaddcf1a98fb69e5737e96e2c1e3e2
Adam Smith <coloradoasmith@gmail.com>
2020-10-17 12:45:08 -0600

    adding Context

--- src/App.js
+++ src/App.js
@@ -9,82 +9,86 @@ import ChoreList from "./ChoreList/chore-list";
 import Results from "./Results/results";
 
 export default class App extends React.Component {
-  state = {
-    randomIds: [],
-    newChore: "",
-    newChild: "",
-    children: [
-      { id: 1, name: "Larry" },
-      { id: 2, name: "Curly" },
-      { id: 3, name: "Moe" },
-    ],
-    chores: [
-      {
-        id: 1,
-        title: "Mop",
-        child_id: 2,
-        status: false,
-        comments: "",
-      },
-      {
-        id: 2,
-        title: "Vacuum",
-        child_id: 2,
-        status: true,
-        comments: "Good I guess",
-      },
-      {
-        id: 3,
-        title: "Dust",
-        child_id: 2,
-        status: true,
-        comments: "",
-      },
-      {
-        id: 4,
-        title: "Feed the chickens",
-        child_id: 1,
-        status: true,
-        comments: "Nice work",
-      },
-      {
-        id: 5,
-        title: "Trash",
-        child_id: 1,
-        status: false,
-        comments: "",
-      },
-      {
-        id: 6,
-        title: "Mow the grass",
-        child_id: 1,
-        status: false,
-        comments: "",
-      },
-      {
-        id: 7,
-        title: "Polish silver",
-        child_id: 3,
-        status: true,
-        comments: "",
-      },
-      {
-        id: 8,
-        title: "Empty cat box",
-        child_id: 3,
-        status: false,
-        comments: "",
-      },
-      {
-        id: 9,
-        title: "Paint the chicken coop",
-        child_id: 3,
-        status: true,
-        comments: "",
-      },
-    ],
-  };
+  static contextType = Context;
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      randomIds: [],
+      newChore: "",
+      newChild: "",
+      children: [
+        { id: 1, name: "Larry" },
+        { id: 2, name: "Curly" },
+        { id: 3, name: "Moe" },
+      ],
+      chores: [
+        {
+          id: 1,
+          title: "Mop",
+          child_id: 2,
+          status: false,
+          comments: "",
+        },
+        {
+          id: 2,
+          title: "Vacuum",
+          child_id: 2,
+          status: true,
+          comments: "Good I guess",
+        },
+        {
+          id: 3,
+          title: "Dust",
+          child_id: 2,
+          status: true,
+          comments: "",
+        },
+        {
+          id: 4,
+          title: "Feed the chickens",
+          child_id: 1,
+          status: true,
+          comments: "Nice work",
+        },
+        {
+          id: 5,
+          title: "Trash",
+          child_id: 1,
+          status: false,
+          comments: "",
+        },
+        {
+          id: 6,
+          title: "Mow the grass",
+          child_id: 1,
+          status: false,
+          comments: "",
+        },
+        {
+          id: 7,
+          title: "Polish silver",
+          child_id: 3,
+          status: true,
+          comments: "",
+        },
+        {
+          id: 8,
+          title: "Empty cat box",
+          child_id: 3,
+          status: false,
+          comments: "",
+        },
+        {
+          id: 9,
+          title: "Paint the chicken coop",
+          child_id: 3,
+          status: true,
+          comments: "",
+        },
+      ],
+    };
+  }
   updateChoreChildId(childId, choreId) {
     if (childId) {
       let result = this.state.children.filter((obj) => {
